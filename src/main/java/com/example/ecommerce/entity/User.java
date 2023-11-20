ad296c13edbf6947f1f487834cb45ba9400f0781
Anton Tapper Dock <at@appsson.se>
2023-11-20 23:00:30 +0100

    database connection

--- src/main/java/com/example/ecommerce/entity/User.java
+++ src/main/java/com/example/ecommerce/entity/User.java
@@ -1,4 +1,32 @@
 package com.example.ecommerce.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
