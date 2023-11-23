c9d0131018a52a42dc5fe80830089664a7133c3b
Anton Tapper Dock <at@appsson.se>
2023-11-23 14:33:42 +0100

    Implemented a custom UserDetailsService to load user details

--- src/main/java/com/example/ecommerce/entity/User.java
+++ src/main/java/com/example/ecommerce/entity/User.java
@@ -35,4 +35,32 @@ public class User {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<com.example.ecommerce.entity.Role> getRoles() {
+        return roles;
+    }
 }
